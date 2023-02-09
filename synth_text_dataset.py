cc1d0534b3e7f928546bb130cdb24507c17e87b1
Ori Dabush <dabushori@gmail.com>
2023-02-09 09:55:18 +0200

    Cleaned the dataset code

--- synth_text_dataset.py
+++ synth_text_dataset.py
@@ -1,8 +1,8 @@
 from torch.utils.data import Dataset
-import torch
 import h5py
 import numpy as np
 import cv2 as cv
+from collections.abc import Callable
 
 
 class SynthTextCharactersDataset(Dataset):
@@ -21,11 +21,12 @@ class SynthTextCharactersDataset(Dataset):
         end_idx: int = None,
         shape: tuple[int] = (100, 200),
         train: bool = True,
-        transform=None,
-        target_transform=None,
-        on_get_item_transform=None,
-        on_get_item_target_transform=None,
-    ):
+        transform: Callable[[np.array], np.array] | None = None,
+        target_transform: Callable[[np.array], np.array] | None = None,
+        on_get_item_transform: Callable[[np.array], np.array] | None = None,
+        on_get_item_target_transform: Callable[[np.array], np.array] | None = None,
+        full_image_transform: Callable[[np.array], np.array] | None = None,
+    ) -> None:
         """
         Create the dataset. The items that will be saved are the characters, each item is a character from an image.
         If this is a train dataset, each item will be saved as ((img_name, charBB), font).
@@ -34,12 +35,14 @@ class SynthTextCharactersDataset(Dataset):
         The shape will be the shape of each character's image after the projective transform.
 
         The `transform` and `target_transform` are applied to the input vector and its label on the initialization.
-        The `on_get_item_transform` `on_get_item_target_transform` are applied to the input vector and its label on a call to get item.
-        These 2 are used to save RAM usage and avoid loading all the dataset to the RAM.
+        The `on_get_item_transform` and `on_get_item_target_transform` are applied to the input vector and its label on a call to get item.
+        The `full_image_transform` is applied to the whole image, before the character is taken from it.
+        These transforms are used to save RAM usage and avoid loading all the dataset to the RAM.
         """
 
         self.on_get_item_transform = on_get_item_transform
         self.on_get_item_target_transform = on_get_item_target_transform
+        self.full_image_transform = full_image_transform
 
         self.db = h5py.File(filename, "r")
         self.db_data = self.db["data"]
@@ -60,9 +63,11 @@ class SynthTextCharactersDataset(Dataset):
                     # and the third axis is the index of the character.
                     charBB = curr_img_data.attrs["charBB"][:, :, idx]
 
+                    # Get the label and apply the `target_transform` on it (if available)
                     font = curr_img_data.attrs["font"][idx]
                     font = target_transform(font) if target_transform else font
 
+                    # Get the input vector and apply the `transform` on it (if available)
                     x = (im, charBB)
                     x = transform(x) if transform else x
 
@@ -78,19 +83,20 @@ class SynthTextCharactersDataset(Dataset):
                     # and the third axis is the index of the character.
                     charBB = curr_img_data.attrs["charBB"][:, :, idx]
 
+                    # Get the input vector and apply the `transform` on it (if available)
                     x = (im, charBB)
                     x = transform(x) if transform else x
 
                     self.items.append(((im, charBB)))
 
-    def __len__(self):
+    def __len__(self) -> int:
         """
         Get the length of the dataset
         """
 
         return len(self.items)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> tuple[np.array, any] | tuple[np.array]:
         """
         Get an item from the dataset
         """
@@ -101,34 +107,48 @@ class SynthTextCharactersDataset(Dataset):
         else:
             x = self.items[idx]
 
-        # Get the image of the chatacter (after the projective transform)
-        x = self.get_char_data(x[0], x[1], self.shape).float()
+        # Get the chatacter out of the image (using a projective transform)
+        x = self.get_char_data(x[0], x[1], self.shape)
+
         # The images are given as (h,w,c), and pytorch requires (c,h,w)
-        x = x.permute((2, 0, 1))
-        # Perform the specified "on get item transform"
+        if len(x.shape) == 3:
+            x = x.swapaxes(1, 2).swapaxes(0, 1)
+
+        # Apply the specified `on_get_item_transform` (if available)
         x = self.on_get_item_transform(x) if self.on_get_item_transform else x
 
-        # Perform the specified "on get item target transform"
+        # Apply the specified `on_get_item_target_transform` (if available)
         y = (
             self.train and self.on_get_item_target_transform(y)
             if self.on_get_item_target_transform
             else y
         )
+
         return (x, y) if self.train else x
 
-    def get_image_data(self, img_name: str):
+    def get_item_raw(self, idx) -> tuple[tuple[np.array, np.array], any]:
+        """
+        Get an raw item from the dataset
+        """
+
+        return self.items[idx]
+
+    def get_image_data(self, img_name: str) -> np.array:
         """
         Get an image given it's name
         """
 
+        # Apply the `full_image_transform` if available
+        if self.full_image_transform:
+            return self.full_image_transform(self.db_data[img_name][:])
         return self.db_data[img_name][:]
 
-    def get_char_data(self, img_name: str, charBB, shape: tuple[int]):
+    def get_char_data(self, img_name: str, charBB, shape: tuple[int]) -> np.array:
         """
         Get an image of a character given the full image's name and it's bounding box using a prjective transform
         """
 
-        img = self.db_data[img_name][:]
+        img = self.get_image_data(img_name)
 
         src_points = np.array(
             list(charBB[:, i] for i in range(charBB.shape[1])), dtype=np.float32
@@ -141,4 +161,4 @@ class SynthTextCharactersDataset(Dataset):
         proj_matrix = cv.getPerspectiveTransform(src_points, dst_points)
         out_img = cv.warpPerspective(img, proj_matrix, shape)
 
-        return torch.from_numpy(out_img)
+        return out_img
